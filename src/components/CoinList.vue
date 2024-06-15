37257ab8291ed54dd42930547978b09f4024d350
Jaehoon Kim <46511592+redman1234321@users.noreply.github.com>
2024-06-16 02:06:37 +0900

    Create CoinList.vue

---
+++ src/components/CoinList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Coin List</h2>
+    <ul>
+      <li v-for="(coin, index) in coins" :key="index">
+        {{ coin.name }}: {{ coin.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    coins: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
