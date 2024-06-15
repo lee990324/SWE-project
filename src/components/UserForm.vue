05f9d66dad5ae5f0fdd54e7204cb95bf170b9b4f
Jaehoon Kim <46511592+redman1234321@users.noreply.github.com>
2024-06-16 02:05:58 +0900

    Create UserForm.vue

---
+++ src/components/UserForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label for="id">User ID:</label>
+      <input type="text" v-model="user.id" id="id" required>
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" v-model="user.password" id="password" required>
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        id: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', this.user);
+      this.user.id = '';
+      this.user.password = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
