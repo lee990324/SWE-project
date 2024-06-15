afc3ddaf51c513c787f4324150797a507c365931
Jaehoon Kim <46511592+redman1234321@users.noreply.github.com>
2024-06-16 02:12:43 +0900

    Create CoinForm.vue

---
+++ src/components/CoinForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label for="coin">Coin:</label>
+      <input type="text" v-model="coin.name" id="coin" required>
+    </div>
+    <div>
+      <label for="price">Price:</label>
+      <input type="number" v-model="coin.price" id="price" required>
+    </div>
+    <button type="submit">Add Coin</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      coin: {
+        name: '',
+        price: 0
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', this.coin);
+      this.coin.name = '';
+      this.coin.price = 0;
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
