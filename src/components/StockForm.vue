53244bb306b415e0de984a98898bdb8848a3ebb7
Jaehoon Kim <46511592+redman1234321@users.noreply.github.com>
2024-06-16 23:13:24 +0900

    Create StockForm.vue
    form for stocks

---
+++ src/components/StockForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label for="stock">Stock Symbol:</label>
+      <input type="text" v-model="stock.symbol" id="stock" required>
+    </div>
+    <button type="submit">Add Stock</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stock: {
+        symbol: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('submit', this.stock);
+      this.stock.symbol = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
